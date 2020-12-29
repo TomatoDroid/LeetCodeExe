1472960e04b1ec231a4b6029f90de670aa847f1a
zhen <770226915@qq.com>
2020-12-30 07:56:07 +0800

    n皇后

--- 52.js
+++ 52.js
@@ -1,59 +1,37 @@
 /**
  * @param {number} n
- * @return {string[][]}
+ * @return {number}
  */
-var solveNQueens = function (n) {
-  if (n < 1) {
-    return [];
-  }
-
-  const result = [];
-  const cols = new Set();
-  const pie = new Set();
-  const na = new Set();
-  _DFS(n, 0, [], result, cols, pie, na);
-  return generate(n, result)
-};
-
-function _DFS(n, row, currentState, result, cols, pie, na) {
-  if (row >= n) {
-    result.push(currentState);
-    return;
-  }
-  for (let col = 0; col < n; col++) {
-    if (cols.has(col) || pie.has(row + col) || na.has(row - col)) {
-        continue
+var totalNQueens = function(n) {
+    if (n < 1) {
+        return 0
     }
-    cols.add(col)
-    pie.add(row + col)
-    na.add(row - col)
-
-    _DFS(n, row + 1, currentState+[col], result, cols, pie, na)
-
-    cols.delete(col)
-    pie.delete(row + col)
-    na.delete(row - col)
-  }
+    const result = []
+    const cols = new Set()
+    const pie = new Set()
+    const na = new Set()
+    _DFS(n, 0, result, [], cols, pie, na)
+    return result.length
 }
 
-function generate(n, result) {
-    const res = []
-    for (let board of result) {
-        const item = []
-        for (let rowTemp of board) {
-            let row = ''
-            for (let col = 0; col < n; col++) {
-                if (+rowTemp === col ) {
-                    row += 'Q'
-                } else {
-                    row += '.'
-                }
-            }
-            item.push(row)
+function _DFS(n, row, result, currentState, cols, pie, na) {
+    if (row >= n) {
+        result.push(currentState)
+        return
+    }
+    for (let col = 0; col < n; col++) {
+        if (cols.has(col) || pie.has(col + row) || na.has(col - row)) {
+            continue
         }
-        res.push(item)
+        cols.add(col)
+        pie.add(col + row)
+        na.add(col - row)
+        _DFS(n, row + 1, result, currentState + [col], cols, pie, na)
+
+        cols.delete(col)
+        pie.delete(col + row)
+        na.delete(col - row)
     }
-    return res
 }
 
-console.log(JSON.stringify(solveNQueens(4)));
+console.log(totalNQueens(4));

d82efb4b8ce4c26f8a050184e2d66826d569ab66
zhen <770226915@qq.com>
2021-01-22 11:15:03 +0800

    2的幂

---
+++ 231.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ * 时间复杂度 O(logN)
+ */
+var isPowerOfTwo = function (n) {
+  let s = 1;
+  while (s < n) {
+    if (s < 0) {
+      return false;
+    }
+    s = s << 1;
+  }
+  return n === s;
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ * 时间复杂度 O(1)
+ * 只有2的幂次方符合 =》 x & -x === x
+ */
+var isPowerOfTwo = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+  return (n & -n) === n;
+};
+/**
+ * @param {number} n
+ * @return {boolean}
+ * 时间复杂度 O(1)
+ * 只有2的幂次方符合 =》 x & (x-1) === 0
+ */
+var isPowerOfTwo = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+  return (x & (x - 1)) === 0;
+};
+// console.log(isPowerOfTwo(1));
+// console.log(isPowerOfTwo(16));
+// console.log(isPowerOfTwo(218));
+console.log(isPowerOfTwo(1073741825));
